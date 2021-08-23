6231d385392b381b904ba248960bb2c7ce51091d
m <m@ms-MacBook-Pro.local>
2021-08-23 14:40:44 +0200

    220821

--- journalist/journalist.py
+++ journalist/journalist.py
@@ -1,6 +1,8 @@
 import argparse
 from datetime import date
 
+# https://github.com/XXIIVV/oscean/blob/master/src/database/journal0005.tbtl
+
 OUTJOURNAL_FILE_PATH = "journal_out.txt"
 INJOURNAL_FILE_PATH = "journal_in.txt"
 
@@ -42,37 +44,37 @@ def date_type(s):
 
 # Add the arguments
 parser.add_argument('activity_code',
-                   	 metavar='DD',
+                   	 metavar='activity',
                    	 type=activity_type,
-                     help="the code for the activity as 2 digits[DD]")
+                     help="the code for the activity")
 
 parser.add_argument('host',
-                      metavar='HOST',
+                      metavar='host',
                       type=host_type,
                       action='store',
                       help='the host of the project')
 
 parser.add_argument('pomodoros',
-                   	 metavar='D',
+                   	 metavar='sessions',
                      type=float,
                      help="amount of pomodoros spent on the activity")
 
 parser.add_argument('total',
-                      metavar='D',
+                      metavar='time assigned',
                       type=float,
                       action='store',
                       help='total time spent working on the project in hours')
 
 parser.add_argument('-nm',
                      '--name',
-                      metavar='NAME',
+                      metavar='entry name',
                       type=name_type,
                       action='store',
                       help='the name of the project of the day')
 
 parser.add_argument('-im',
                      '--image',
-                      metavar='IMAGE_NAME',
+                      metavar='image name',
                       type=str,
                       action='store',
                       help='the name of associated project image')
@@ -81,19 +83,19 @@ parser.add_argument('-in',
                      '--is_input',
                      default=False,
                      action='store_true',
-                     help='is the project input entry?')
+                     help='is this input entry?')
 
 parser.add_argument('-dt',
                      '--date',
-                     metavar='DDMOYR',
+                     metavar='project date',
                      type=date_type,
                      action='store',
                      help='force custom date')
 
 parser.add_argument('-pi',
                      '--prod_index',
-                     metavar='D.D',
-                     type=date_type,
+                     metavar='productivity index',
+                     type=float,
                      action='store',
                      help='force productivity index')
 
@@ -118,13 +120,18 @@ else:
 
 
 activity_entry = args.activity_code
-host_entry = args.host
+
+if(activity_entry == "00"):
+	host_entry = ""
+else:
+	host_entry = args.host
+
 productivity_entry = productivity_index
 total_entry = args.total 
 name_entry = args.name
 image_entry = args.image
 
-entry = "{0} {1}       {2}  {3}  {4}".format(date_entry, activity_entry, productivity_entry, total_entry, host_entry)
+entry = "{0} {1}       {2}  {3}   {4}".format(date_entry, activity_entry, productivity_entry, total_entry, host_entry)
 
 if(args.name):
 	padding = ' ' * (HOST_ENTRY_MAX_CHARS - len(args.host) + 1)
